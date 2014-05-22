7393e0af9f45978fed845999dc3518a6964d3dba
Peter Barnes <barnespeter213@gmail.com>
2014-05-22 00:13:07 -0500

    Create static pages and complete header

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,12 @@ class StaticPagesController < ApplicationController
   def about
     
   end
+
+  def projects
+    
+  end
+
+  def resume
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact',  to: 'static_pages#contact',  via: 'get'
+  match '/about',    to: 'static_pages#about',    via: 'get'
+  match '/projects', to: 'static_pages#projects', via: 'get'
+  match '/resume',   to: 'static_pages#resume',   via: 'get'
 end
